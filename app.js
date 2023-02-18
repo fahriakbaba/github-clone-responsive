0b9b2fccc029615ef6f039c25a8e459bcd9b560f
Fahri Akbaba <fahriakbaba@gmail.com>
2023-02-18 19:34:25 +0300

    added js function for responsive navbar menu

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const hamburgerIcon = document.querySelector("#hamburger");
+const nav = document.querySelector("nav");
+const content = document.querySelector(".content");
+
 window.addEventListener("scroll", showStickyLeft);
 
 function showStickyLeft() {
     if (window.scrollY > 300) {
         document.querySelector(".sticky-left").style.opacity = "1";
         document.querySelector(".sticky").style.gridTemplateRows = "50px";
+        nav.style.display = "none";
     }
     else {
         document.querySelector(".sticky-left").style.opacity = "0";
@@ -12,12 +16,15 @@ function showStickyLeft() {
     }
 }
 
-const nav = document.querySelector("nav");
-const content = document.querySelector(".content");
-
 hamburgerIcon.addEventListener("click", toggleClick);
 
 function toggleClick() {
     nav.style.display === "block" ? nav.style.display = "none" : nav.style.display = "block";
-    content.style.marginTop === "100px" ? content.style.marginTop = "0px" : content.style.marginTop = "100px";
+    content.style.marginTop === "185px" ? content.style.marginTop = "0px" : content.style.marginTop = "185px";
 }
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 1199) {
+        nav.style.display = "none";
+    }
+})
